7d1947482f7fff81cd06f8a50d0d256eaa221d68
DOSTI GAME STUDIO <86553407+DOSTI-99999@users.noreply.github.com>
2021-07-11 16:36:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,8 +20,7 @@ def get_new_memes():
     Returns:
         imgs [list]: List of image URLs
     """
-    url = 'https://www.memedroid.com/memes/tag/programming'
-          'https://www.memedroid.com/memes/tag/minecraft'
+    url = 'https://www.memedroid.com/memes/tag/minecraft'
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'lxml')
     divs = soup.find_all('div', class_='item-aux-container')
